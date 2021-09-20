8fe30368ee635169cad4b8a37240922abf854451
onur can <can.yondem.22@gmail.com>
2021-09-21 01:09:53 +0300

    added responsive language setting buttons

--- components/HamburgerMenu.jsx
+++ components/HamburgerMenu.jsx
@@ -1,78 +1,96 @@
-import Head from 'next/head'
-import { useState, useEffect } from 'react';
-import { FaHamburger} from 'react-icons/fa';
-import {motion} from "framer-motion";
+import { useState, useEffect, useContext } from 'react';
+import { FaHamburger } from 'react-icons/fa';
+import { motion } from "framer-motion";
 import Link from 'next/link';
+import LangContext from '../context/LangContext';
+import ReactCountryFlag from "react-country-flag"
+
 
 function useWindowSize() {
-    // Initialize state with undefined width/height so server and client renders match
-    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-    const [windowSize, setWindowSize] = useState({
-      width: undefined,
-      height: undefined,
-    });
-  
-    useEffect(() => {
-      // only execute all the code below in client side
-      if (typeof window !== 'undefined') {
-        // Handler to call on window resize
-        function handleResize() {
-          // Set window width/height to state
-          setWindowSize({
-            width: window.innerWidth,
-            height: window.innerHeight,
-          });
-        }
-      
-        // Add event listener
-        window.addEventListener("resize", handleResize);
-       
-        // Call handler right away so state gets updated with initial window size
-        handleResize();
-      
-        // Remove event listener on cleanup
-        return () => window.removeEventListener("resize", handleResize);
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    if (typeof window !== 'undefined') {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
       }
-    }, []); // Empty array ensures that effect is only run on mount
-    return windowSize;
-  }
+
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
 
 export default function HamburgerMenu() {
-    const size = useWindowSize();
-    const [isActive,setIsActive] = useState(false);
+  const size = useWindowSize();
+  const [isActive, setIsActive] = useState(false);
+  const { setLanguage, lang } = useContext(LangContext);
   return (
     <div>
-      <motion.div className="md:hidden ml-16 absolute z-10 flex" drag dragConstraints={{ left: 0 ,right:size.width-160, top:0, bottom:size.height-60}} onClick={() => setIsActive(!isActive)}>
-            <FaHamburger className="text-3xl mr-5"/>
-            {isActive ?  
-                    <ul className="flex flex-col mt-5 bg-gray-600 p-4 text-white font-bold rounded-lg text-center ">
-                        <li className="p-3 border-b-2 border-gray-50">
-                            <Link href="/">
-                                <a>HOME</a>
-                            </Link>
-                        </li>
-                        <li className="p-3 border-b-2 border-gray-50">
-                            <Link href="/about">
-                                <a>ABOUT</a>
-                            </Link>
-                        </li>
-                        <li className="p-3 border-b-2 border-gray-50">
-                            <Link href="/skills">
-                                <a>SKİLL</a>
-                            </Link>
-                        </li>
-                        <li className="p-3 border-b-2 border-gray-50">
-                            <Link href="/projects">
-                                <a>PROJECT</a>
-                            </Link>
-                        </li>
-                        <li className="p-3">
-                            <Link href="/contact">
-                                <a>CONTACT</a>
-                            </Link>
-                        </li>
-                    </ul> : null}
-        </motion.div>
+      <motion.div className="md:hidden ml-16 absolute z-10 flex" drag dragConstraints={{ left: 0, right: size.width - 160, top: 0, bottom: size.height - 60 }} onClick={() => setIsActive(!isActive)}>
+        <FaHamburger className="text-3xl mr-5" />
+        {isActive ?
+          <ul className="flex flex-col mt-5 bg-gray-600 p-4 text-white font-bold rounded-lg text-center ">
+            <li className="p-3 border-b-2 border-gray-50">
+              <Link href="/">
+                <a>{lang.home}</a>
+              </Link>
+            </li>
+            <li className="p-3 border-b-2 border-gray-50">
+              <Link href="/about">
+                <a>{lang.about}</a>
+              </Link>
+            </li>
+            <li className="p-3 border-b-2 border-gray-50">
+              <Link href="/skills">
+                <a>{lang.skills}</a>
+              </Link>
+            </li>
+            <li className="p-3 border-b-2 border-gray-50">
+              <Link href="/projects">
+                <a>{lang.projects}</a>
+              </Link>
+            </li>
+            <li className="p-3 border-b-2 border-gray-50">
+              <Link href="/contact">
+                <a>{lang.contact}</a>
+              </Link>
+            </li>
+            <li className="flex justify-center items-center gap-4 mt-3">
+              <button onClick={() => setLanguage("en")}>
+                <ReactCountryFlag countryCode="GB" svg style={{
+                  fontSize: '2em',
+                  lineHeight: '2em',
+                }} />
+              </button>
+              <button onClick={() => setLanguage("tr")}>
+                <ReactCountryFlag countryCode="TR" svg style={{
+                  fontSize: '2em',
+                  lineHeight: '2em',
+                }} />
+              </button>
+            </li>
+          </ul> : null}
+      </motion.div>
+
     </div>
   )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,14 +8,14 @@ import { useRouter } from "next/router"
 
 
 export default function Home({ page, textcolor }) {
-    const {setLanguage, lang} = useContext(LangContext);
+    const { setLanguage, lang } = useContext(LangContext);
     const router = useRouter();
 
     return (
         <>
             <motion.nav className={page === "main" ? "md:flex md:justify-center py-5 absolute z-10 justify-start w-full hidden" : " md:flex md:justify-center justify-start py-5 hidden"} animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -50 }} transition={{ duration: 1 }}>
                 <div className="flex">
-                    <ul className={`flex gap-x-7 font-bold ${textcolor}`}>
+                    <ul className={`flex gap-x-7 font-bold ${textcolor} `}>
                         <li className="hover:text-green-400 transition ease-in-out duration-300 md:block hidden">
                             <Link href="/">
                                 <a className={router.pathname === "/" && "border-b-2 border-green-500 pb-2"}>{lang.home}</a>
@@ -41,12 +41,15 @@ export default function Home({ page, textcolor }) {
                                 <a className={router.pathname === "/contact" && "border-b-2 border-green-500 pb-2"}>{lang.contact}</a>
                             </Link>
                         </li>
+                        <li>
+                            <select name="languages" id="languages" onChange={(e) => setLanguage(e.target.value)} className="bg-transparent text-gray-400 outline-none">
+                                <option value="tr">Türkçe</option>
+                                <option value="en">English</option>
+                            </select>
+                        </li>
                     </ul>
                 </div>
-                <select name="languages" id="languages" onChange={(e) => setLanguage(e.target.value)} className="bg-transparent text-gray-400 absolute right-56 outline-none">
-                    <option value="tr">Türkçe</option>
-                    <option value="en">English</option>
-                </select>
+
             </motion.nav>
             <HamburgerMenu />
         </>
