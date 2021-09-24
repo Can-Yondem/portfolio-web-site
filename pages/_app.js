2bbe98e5826258a92ab4dc97f4ec22988d26ad36
onur can <can.yondem.22@gmail.com>
2021-09-25 01:21:26 +0300

    added next-intl again

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import 'tailwindcss/tailwind.css'
 import Layout from '../components/Layout'
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
-import { NextIntlProvider } from 'Next-intl';
+import { NextIntlProvider } from 'next-intl';
 
 
 function MyApp({ Component, pageProps }) {
